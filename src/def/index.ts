b9faff8827adcbc7f193b836545043ac1a2b05cf
woldann <serkanaksy@pm.me>
2026-02-19 14:05:41 +0300

    feat: Add `ThreadCreationFlags` enum and export it from the definitions index.

--- src/def/index.ts
+++ src/def/index.ts
@@ -1,3 +1,4 @@
 export * from './access.js';
 export * from './memory.js';
 export * from './context.js';
+export * from './thread.js';
---
+++ src/def/thread.ts
@@ -0,0 +1,21 @@
+/**
+ * Thread creation flags
+ * https://learn.microsoft.com/en-us/windows/win32/procthread/thread-creation-flags
+ */
+export enum ThreadCreationFlags {
+  /**
+   * The thread runs immediately after creation.
+   */
+  IMMEDIATE = 0,
+
+  /**
+   * The thread is created in a suspended state, and does not run until the ResumeThread function is called.
+   */
+  CREATE_SUSPENDED = 0x00000004,
+
+  /**
+   * The dwStackSize parameter specifies the initial reserve size of the stack.
+   * If this flag is not specified, dwStackSize specifies the commit size.
+   */
+  STACK_SIZE_PARAM_IS_A_RESERVATION = 0x00010000,
+}
